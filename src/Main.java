7e31901e24b253a062e728432909a9c741b6791d
Ilya <rabota003@yandex.ru>
2023-01-30 20:59:53 +0300

    Готово

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,16 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        // task1-2-3
+        Car ladaGranta = new Car(" Lada ", " Granta ", 1.7, " yellow ", 2015, " Россия ");
+        Car audi = new Car(" Audi ", " A8 50L TDL quattro ", 3.0, " black ", 2020, " Германия ");
+        Car bmw = new Car(" BMW ", " Z8 ", 3.0, " black ", 2021, " Германия ");
+        Car kia = new Car(" Kia ", " Sportage 4 ", 2.4, " red ", 2018, " Южная Корея ");
+        Car hyundai = new Car(" Hyundai ", " Avante ", 1.6, " red ", 2016, " Южная Корея ");
+
+        ladaGranta.print();
+        audi.print();
+        bmw.print();
+        kia.print();
+        hyundai.print();
     }
 }
